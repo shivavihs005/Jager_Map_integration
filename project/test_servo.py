c37b4de8be98112ea351c7e27054314961a67ecc
Shiva P <149098895+shivavihs005@users.noreply.github.com>
2026-03-05 22:23:33 +0530

    turn manager

--- project/test_servo.py
+++ project/test_servo.py
@@ -1,16 +1,53 @@
 import pigpio
 import time
 
-SERVO_PIN = 18
+SERVO_PIN = 17
+
+MAX_LEFT = 680
+CENTER = 1060
+MAX_RIGHT = 1460
+
+STEP = 10
+DELAY = 0.02
 
 pi = pigpio.pi()
 
-def set_servo(angle):
-    pulse = 500 + (angle / 180) * 2000   # convert angle to pulse width
-    pi.set_servo_pulsewidth(SERVO_PIN, pulse)
-    print(f"Servo set to {angle}°")
+current = CENTER
+pi.set_servo_pulsewidth(SERVO_PIN, CENTER)
 
-set_servo(90)
 
-while True:
-    time.sleep(1)
+def move_to(target):
+    global current
+
+    if target > current:
+        step = STEP
+    else:
+        step = -STEP
+
+    for pulse in range(current, target, step):
+        pi.set_servo_pulsewidth(SERVO_PIN, pulse)
+        time.sleep(DELAY)
+
+    pi.set_servo_pulsewidth(SERVO_PIN, target)
+    current = target
+
+
+try:
+    while True:
+
+        move_to(MAX_LEFT)
+        time.sleep(1)
+
+        move_to(CENTER)
+        time.sleep(1)
+
+        move_to(MAX_RIGHT)
+        time.sleep(1)
+
+        move_to(CENTER)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pi.set_servo_pulsewidth(SERVO_PIN, 0)
+    pi.stop()
+
